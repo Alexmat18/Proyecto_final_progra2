38423f7a78f90299cfb51dee12898f6f371950ff
Alexmat18 <jefers.garcia@teczion.edu,gt>
2025-06-03 09:42:38 -0600

    comentarios

--- main.py
+++ main.py
@@ -136,8 +136,7 @@ while True:
         if salida==1:#si el retorno de la funcion es 1 que significa que el usuario eligio salir, se detiene el bucle principal
             break
     elif validarTipoUsuario(Ingresar_usuario, Ingresar_contraseña)=='Usuario Administrator':
-        print ('USUARIO ES DE TIPO ADMINISTRATOR')
-        #UsuarioAdministrator()
+        print ('USUARIO DE TIPO ADMINISTRATOR')
         salida=UsuarioAdministrator()
         if salida==1:
             break
